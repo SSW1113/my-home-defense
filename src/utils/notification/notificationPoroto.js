82fc4333fbac0feb1cc45f7c87ecff3a88e49971
Jinsu_Mun <answlstn13@gmail.com>
2024-11-08 10:26:56 +0900

    fix - mjs notification 로직에 맞게 수정
    처음 몬스터관련notification프로토 파일 정보를 responseProto.js에 프로토 파일 정보를 저장하였는데 지우고 notificationProto.js에 작성하였습니다.

--- src/utils/notification/notificationPoroto.js
+++ src/utils/notification/notificationPoroto.js
@@ -1,8 +1,16 @@
 import { PacketType } from '../../constants/header.js';
 
 export const notificationProto = {
-    [PacketType.MATCH_START_NOTIFICATION]: {
-        protoType: 'gameNotification.S2CMatchStartNotification',
-        fieldName: 'matchStartNotification',
-    },
+  [PacketType.MATCH_START_NOTIFICATION]: {
+    protoType: 'gameNotification.S2CMatchStartNotification',
+    fieldName: 'matchStartNotification',
+  },
+  [PacketType.SPAWN_ENEMY_MONSTER_NOTIFICATION]: {
+    protoType: 'gameNotification.S2CSpawnEnemyMonsterNotification',
+    fieldName: 'spawnEnemyMonsterNotification',
+  },
+  [PacketType.ENEMY_MONSTER_DEATH_NOTIFICATION]: {
+    protoType: 'gameNotification.S2CEnemyMonsterDeathNotification',
+    fieldName: 'enemyMonsterDeathNotification',
+  },
 };
